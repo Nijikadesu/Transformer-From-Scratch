419075ce19efde08b5029a47d4e921176505531d
mingkun <mingkun502@gmail.com>
2024-09-28 10:36:42 +0800

    implement positional encoding

---
+++ model/positional_encoding.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, max_sequence_length):
+        super().__init__()
+        self.max_sequence_length = max_sequence_length
+        self.d_model = d_model
+
+    def forward(self):
+        even_i = torch.arange(0, self.d_model, 2).float()
+        denominator = torch.pow(10000, even_i / self.d_model)
+        position = torch.arange(self.max_sequence_length).reshape(self.max_sequence_length, 1)
+        even_PE = torch.sin(position / denominator)
+        odd_PE = torch.cos(position / denominator)
+        stacked = torch.stack([even_PE, odd_PE], dim=2)
+        PE = torch.flatten(stacked, start_dim=1, end_dim=2)
+        return PE

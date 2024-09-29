19ab6de08742fe5e46060f84e1a782a246371214
mingkun <mingkun502@gmail.com>
2024-09-29 23:34:24 +0800

    implement multi-head cross attention

---
+++ model/multihead_cross_attention.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+def scaled_dot_product(q, k, v, mask=None):
+    """
+    This function performs a scaled dot product on q, k and v.
+    """
+    d_k = q.shape[-1]
+    scaled = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scaled += mask
+    attention = F.softmax(scaled, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+class MultiheadCrossAttention(nn.Module):
+    """
+    Implementation of the Multihead Cross Attention Mechanism.
+    """
+    def __init__(self, d_model, num_heads):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.head_dim = d_model // num_heads
+        self.kv_layer = nn.Linear(d_model, d_model * 2)
+        self.q_layer = nn.Linear(d_model, d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
+
+    def forward(self, x, y, mask=None):
+        batch_size, sequence_length, input_dim = x.size()
+        kv = self.kv_layer(x)
+        q = self.q_layer(y)
+        kv = kv.reshape(batch_size, sequence_length, self.num_heads, 2 * self.head_dim)
+        q = q.reshape(batch_size, sequence_length, self.num_heads, self.head_dim)
+        kv = kv.permute(0, 2, 1, 3)
+        q = q.permute(0, 2, 1, 3)
+        k, v = kv.chunk(2, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask)
+        values = values.reshape(batch_size, sequence_length, self.d_model)
+        out = self.linear_layer(values)
+        return out

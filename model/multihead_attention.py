2a7bafb8dc3e9a575d16b3f91c7cb6c4001a5145
mingkun <mingkun502@gmail.com>
2024-09-29 23:35:29 +0800

    implement multi-head attention

--- model/multihead_attention.py
+++ model/multihead_attention.py
@@ -5,6 +5,9 @@ import torch.nn.functional as F
 import math
 
 def scaled_dot_product(q, k, v, mask=None):
+    """
+    This function performs scaled dot product on q, k and v.
+    """
     d_k = q.size()[-1]
     # scaled product in order to prevent gradient vanishing
     scaled = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
@@ -15,15 +18,16 @@ def scaled_dot_product(q, k, v, mask=None):
     return values, attention
 
 class MultiheadAttention(nn.Module):
-
-    def __init__(self, input_dim, d_model, num_heads):
+    """
+    Implementation of Multihead Attention Mechanism.
+    """
+    def __init__(self, d_model, num_heads):
         super().__init__()
-        self.input_dim = input_dim
-        self.d_model = d_model
+        self.d_model = d_model # dimension of word embedding vectors.
         self.num_heads = num_heads
         self.head_dim = d_model // num_heads
-        self.qkv_layer = nn.Linear(input_dim, 3 * d_model)
-        self.linear_layer = nn.Linear(d_model, input_dim)
+        self.qkv_layer = nn.Linear(d_model, 3 * d_model)
+        self.linear_layer = nn.Linear(d_model, d_model)
 
     def forward(self, x, mask=None):
         batch_size, sequence_length, input_dim = x.size()
@@ -32,6 +36,6 @@ class MultiheadAttention(nn.Module):
         qkv = qkv.permute(0, 2, 1, 3)
         q, k, v = qkv.chunk(3, dim=-1)
         values, attention = scaled_dot_product(q, k, v, mask)
-        values = values.reshape(batch_size, sequence_length, self.num_heads * self.head_dim),
+        values = values.reshape(batch_size, sequence_length, self.num_heads * self.head_dim)
         out = self.linear_layer(values)
         return out
